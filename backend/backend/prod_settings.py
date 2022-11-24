8624b9ebaeed46954b36ed5882aa48ca3d0e1358
unniznd <aanandunnis@gmail.com>
2022-11-24 11:44:46 +0530

    basic project setup added

---
+++ backend/backend/prod_settings.py
@@ -0,0 +1,28 @@
+import os
+import json
+
+
+with open('secret_key.json') as f:
+    secret = json.load(f)
+    
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+ALLOWED_HOSTS = ["*"]
+
+CORS_ALLOW_HEADERS = ['*']
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": secret['ENGINE'],
+        "NAME": secret['NAME'],
+        "USER": secret['USER'],
+        "PASSWORD":os.environ.get('PASSWORD'),
+        "HOST":secret['HOST'],
+        "PORT": secret['PORT']
+    }
+}

139ceb34bda32f8af27a929010f29cc7a4fdff24
shiyasmohd <shiyasjaseena124@gmail.com>
2022-11-24 12:33:08 +0530

    navbar

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,11 +1,37 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { NextUIProvider } from '@nextui-org/react';
+import Topbar from '../components/Navbar/Navbar';
+import { createTheme, NextUIProvider } from "@nextui-org/react"
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  const lightTheme = createTheme({
+    type: 'light',
+    theme: {
+    }
+  })
+  
+  const darkTheme = createTheme({
+    type: 'dark',
+    theme: {
+    }
+  })
+
+  
   return (
-    <NextUIProvider>
-      <Component {...pageProps} />
-    </NextUIProvider>
+    <NextThemesProvider
+      defaultTheme="system"
+      attribute="class"
+      value={{
+        light: lightTheme.className,
+        dark: darkTheme.className
+      }}
+    >
+      <NextUIProvider>
+        <Topbar/>
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </NextThemesProvider>
   ) 
 }
---
+++ frontend/pages/app.tsx
@@ -0,0 +1,7 @@
+export default function App(){
+    return(
+        <div className="hero-section min-h-screen">
+            ccsdj
+        </div>
+    )
+}
